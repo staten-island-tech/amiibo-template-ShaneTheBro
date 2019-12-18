dce25dc8f760d5d09cdc913ea9b8714d02a4bf5b
ShaneTheBro <50725203+ShaneTheBro@users.noreply.github.com>
2019-12-18 11:46:09 -0500

    update

--- public/index.js
+++ public/index.js
@@ -1,34 +1,57 @@
-const path = require("path");
+// const path = require("path");
 
-//express shrinks things down in node
+// //express shrinks things down in node
 
-const express = require("express");
-const app = express(); //this shows express is a method
+// const express = require("express");
+// const app = express(); //this shows express is a method
 
 
-//define paths for express
-const publicDirectorypath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "..templates/views");
+// //define paths for express
+// const publicDirectorypath = path.join(__dirname, "../public");
+// const partialsPath = path.join(__dirname, "../templates/partials");
+// const viewsPath = path.join(__dirname, "..templates/views");
 
-//setup handbars engine and views location
-app.set("views engine", "hbs"); //telling express/Node to use handbars for templates
-app.set("views", viewsPath); //telling express to get templates from templates/views folder
-app.get("", async(req,res) => {
-    try{
-        res.render("index");
-    }
-    catch (error){
-res.status(500).send("Page not found");
-    }
-    res.send("Thami likes bagels and butter and he smells");
-});
-app.get("/thamisucks", (req,res) => {
-    res.send("thami sucks");
-});
-app.listen(3000, (req,res) => {
-    console.log('listening on port 3000');
-});
+// //setup handbars engine and views location
+// app.set("view engine", "hbs"); //telling express/Node to use handbars for templates
+// app.set("views", viewsPath); //telling express to get templates from templates/views folder
+// hbs.registerPartials(partialsPath);
+// app.get("/swag", async(req,res) => {
+//     try{
+//         res.render("swag");
+//     }
+//     catch (error){
+// res.status(500).send("Page not found");
+//     }
+// });
+// app.get("/thamisucks", (req,res) => {
+//     res.send("thami sucks");
+// });
+// app.listen(3000, (req,res) => {
+//     console.log('listening on port 3000');
+// });
 //127.0.0.1:3000
 
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+const app = express();
 
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath);
+app.get("", async (req, res) => {
+try {
+res.render("index",{
+    title: "Our first express app"
+}
+} catch {
+res.status(500).send();
+}
+});
 
