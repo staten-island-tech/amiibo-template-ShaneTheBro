f4b51d202e3d90fd497d7f3ecbf4af45de7e3e1f
ShaneTheBro <50725203+ShaneTheBro@users.noreply.github.com>
2020-01-03 11:44:58 -0500

    update

--- public/index.js
+++ public/index.js
@@ -39,6 +39,16 @@ app.get("/anything", async (req, res) => {
     res.status(500).send();
   }
 });
+app.get("/showcase/:id", async (req, res) => {
+  const title = req.params.id
+  try {
+    res.render("showcase", {
+      title: `${title}`
+    })
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
